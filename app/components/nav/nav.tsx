11b4d0d3668d0ae54fbdf0862cf70f8b15a6b97c
William Giammona <wgiammona@gmail.com>
2024-02-10 13:44:50 +0200

    imported styles from ./nav.module.css

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,17 +1,20 @@
+import Image from "next/image";
+import styles from "./nav.module.css";
+
 const nav = () => {
   return (
-    <nav>
-      <figure className="nav__logo--img--wrapper">
+    <nav className={styles.nav}>
+      <figure className={styles.nav__logo__img__wrapper}>
         <Image
           src="https://dev.d24jig8s1lr7n9.amplifyapp.com/img/blinker-icon.4f9b2663.png"
           alt="logo"
           width={200}
           height={50}
-          className="nav__logo--img"
+          className={styles.nav__logo__img}
         />
       </figure>
-      <ul className="nav__link--list">
-        <li className="nav__link">HOME</li>
+      <ul className={styles.nav__link__list}>
+        <li className={styles.nav__link}>HOME</li>
       </ul>
     </nav>
   );
