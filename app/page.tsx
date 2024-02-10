b29a68e2f5256e6665d6c8674d7f2c32d3066b71
William Giammona <wgiammona@gmail.com>
2024-02-10 13:19:06 +0200

    added Nav component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Nav from "./components/nav";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>test</h1>
-    </main>
+    <section id="landing-page">
+      <Nav />
+    </section>
   );
 }
