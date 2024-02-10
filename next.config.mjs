7827288aadcfa18863446aa0ef695ffa9ce2d3d3
William Giammona <wgiammona@gmail.com>
2024-02-10 13:32:32 +0200

    added image domain dev.d24jig8s1lr7n9.amplifyapp.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["dev.d24jig8s1lr7n9.amplifyapp.com"],
+  },
+};
 
 export default nextConfig;
