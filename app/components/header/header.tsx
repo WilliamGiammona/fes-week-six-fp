c23885408722f27d61327cc62afd7796a087a8d1
William Giammona <wgiammona@gmail.com>
2024-02-10 13:52:20 +0200

    fixed icon issue

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,80 +1,82 @@
 import styles from "./header.module.css";
 
-// const header = () => {
-//   const movieChoice = async (event) => {
-//     event.preventDefault();
-//     const movieInput = event.target.elements["movieSearch"];
-//     let movieList = movieInput.value;
+const header = () => {
+  // const movieChoice = async (event) => {
+  //   event.preventDefault();
+  //   const movieInput = event.target.elements["movieSearch"];
+  //   let movieList = movieInput.value;
 
-//     movieInput.value = "";
+  //   movieInput.value = "";
 
-//     const spinner = document.querySelector(".hero__movies--loading");
-//     spinner.style.display = "block";
+  //   const spinner = document.querySelector(".hero__movies--loading");
+  //   spinner.style.display = "block";
 
-//     try {
-//       const response = await fetch(
-//         `https://www.omdbapi.com/?apikey=897b61cc&s=${movieList}`
-//       );
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
+  //   try {
+  //     const response = await fetch(
+  //       `https://www.omdbapi.com/?apikey=897b61cc&s=${movieList}`
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
 
-//       const data = await response.json();
+  //     const data = await response.json();
 
-//       // Check if movies are found
-//       if (!data.Search || data.Search.length === 0) {
-//         const movieWrapper = document.querySelector(".hero__movies--wrapper");
-//         movieWrapper.innerHTML =
-//           "<p>Sorry, no movies with this title found.</p>";
-//         spinner.style.display = "none";
-//         return;
-//       }
+  //     // Check if movies are found
+  //     if (!data.Search || data.Search.length === 0) {
+  //       const movieWrapper = document.querySelector(".hero__movies--wrapper");
+  //       movieWrapper.innerHTML =
+  //         "<p>Sorry, no movies with this title found.</p>";
+  //       spinner.style.display = "none";
+  //       return;
+  //     }
 
-//       let movieTitles = data.Search.map((movie) => movie.Title);
-//       let movieYears = data.Search.map((movie) => movie.Year);
-//       let moviePoster = data.Search.map((movie) => movie.Poster);
+  //     let movieTitles = data.Search.map((movie) => movie.Title);
+  //     let movieYears = data.Search.map((movie) => movie.Year);
+  //     let moviePoster = data.Search.map((movie) => movie.Poster);
 
-//       const movieWrapper = document.querySelector(".hero__movies--wrapper");
-//       movieWrapper.innerHTML = "";
+  //     const movieWrapper = document.querySelector(".hero__movies--wrapper");
+  //     movieWrapper.innerHTML = "";
 
-//       for (let i = 0; i < Math.min(movieTitles.length, 6); i++) {
-//         movieWrapper.innerHTML += `<div class="hero__movies--movie">
-//                         <figure class="hero__movies--movie--poster--img--wrapper">
-//                             <img src="${moviePoster[i]}" alt="movie poster" class="hero__movies--movie--poster--img" />
-//                         </figure>
-//                         <h2 class="hero__movies--title">${movieTitles[i]}</h2>
-//                         <h3 class="hero__movies--year">${movieYears[i]}</h3>
-//                     </div>`;
-//       }
+  //     for (let i = 0; i < Math.min(movieTitles.length, 6); i++) {
+  //       movieWrapper.innerHTML += `<div class="hero__movies--movie">
+  //                       <figure class="hero__movies--movie--poster--img--wrapper">
+  //                           <img src="${moviePoster[i]}" alt="movie poster" class="hero__movies--movie--poster--img" />
+  //                       </figure>
+  //                       <h2 class="hero__movies--title">${movieTitles[i]}</h2>
+  //                       <h3 class="hero__movies--year">${movieYears[i]}</h3>
+  //                   </div>`;
+  //     }
 
-//       spinner.style.display = "none";
+  //     spinner.style.display = "none";
 
-//       return data;
-//     } catch (error) {
-//       console.error("Error fetching data from OMDB API:", error);
-//       spinner.style.display = "none";
-//     }
-//   };
+  //     return data;
+  //   } catch (error) {
+  //     console.error("Error fetching data from OMDB API:", error);
+  //     spinner.style.display = "none";
+  //   }
+  // };
 
-//   return (
-//     <header className="hero">
-//       <h1 className="hero__title">Browse Our Movies</h1>
-//       <form action="" className="hero__form" onSubmit="movieChoice(event);">
-//         <label htmlFor="movieSearch" className="form__label"></label>
-//         <input
-//           type="text"
-//           id="movieSearch"
-//           className="form__input"
-//           placeholder="Search by Title"
-//         />
-//         <button type="submit" className="form__submit--btn">
-//           Submit
-//         </button>
-//       </form>
-//       <i className="fas fa-spinner hero__movies--loading"></i>
-//       <div className="hero__movies--wrapper"></div>
-//     </header>
-//   );
-// };
+  return (
+    <header className={styles.hero}>
+      <h1 className={styles.hero__title}>Browse Our Movies</h1>
+      <form className={styles.hero__form}>
+        <label htmlFor="movieSearch" className={styles.form__label}></label>
+        <input
+          type="text"
+          id="movieSearch"
+          className={styles.form__input}
+          placeholder="Search by Title"
+        />
+        <button type="submit" className={styles.form__submit__btn}>
+          Submit
+        </button>
+      </form>
+      <i
+        className={`${styles.fas} ${styles.faSpinner} ${styles.hero__movies__loading}`}
+      ></i>
+      <div className={styles.hero__movies__wrapper}></div>
+    </header>
+  );
+};
 
-// export default header;
+export default header;
