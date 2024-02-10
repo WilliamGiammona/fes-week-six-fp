2f40af61598c2c84c4d5ef3790235453f3147f2b
William Giammona <wgiammona@gmail.com>
2024-02-10 13:16:03 +0200

    finished nav

---
+++ app/components/nav.tsx
@@ -0,0 +1,18 @@
+const nav = () => {
+  return (
+    <nav>
+      <figure className="nav__logo--img--wrapper">
+        <img
+          src="https://dev.d24jig8s1lr7n9.amplifyapp.com/img/blinker-icon.4f9b2663.png"
+          alt="logo"
+          className="nav__logo--img"
+        />
+      </figure>
+      <ul className="nav__link--list">
+        <li className="nav__link">HOME</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default nav;
